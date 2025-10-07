34bdae4c789c2afa505b7133e5a863f09445aeac
Amit Kumar Saha <mail2amit.saha@yahoo.com>
2025-10-07 22:06:07 +0100

    DecisionTree

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,9 @@
 import pandas
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
-df = pandas.read_csv("data.csv")
+df = pandas.read_csv("DecisionTreeData.csv")
 
 d = {'UK': 0, 'USA': 1, 'N': 2}
 df['Nationality'] = df['Nationality'].map(d)
@@ -18,3 +19,9 @@ dtree = DecisionTreeClassifier()
 dtree = dtree.fit(X, y)
 
 tree.plot_tree(dtree, feature_names=features)
+plt.show()
+
+print(dtree.predict([[40, 10, 7, 1]]))
+
+print("[1] means 'GO'")
+print("[0] means 'NO'")
